f1c940fdcbb1042150b77b7ed6874fb72b8c6764
Roger Silva <silvara99@outlook.com>
2024-01-31 14:49:30 -0500

    Renombre de archivos de DañosDAL a reporte, evitar confusión con solocitud de probadores

--- BackPedidosAtt/server.js
+++ BackPedidosAtt/server.js
@@ -38,6 +38,27 @@ app.post("/save-excel", (req, res) => {
   console.log(formattedDate + " - Pedido " + marca + " de " + user + " solicitado por " + por + " guardado en el servidor");
 });
 
+app.post("/save-excel-danos", (req, res) => {
+  const data = req.body.data;
+  const user = req.body.user;
+  const marca = req.body.marca;
+  const por = req.body.por;
+  const worksheet = XLSX.utils.json_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+  // Obtener la fecha actual y formatearla como una cadena
+  const date = new Date();
+  const formattedDate = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()}`;
+
+  XLSX.writeFile(workbook, path.join(outputDirectory, `${formattedDate}-${user}-${marca}-${por}.xlsx`));
+  res.send("Excel guardado en el servidor!");
+  console.log(formattedDate + " - Pedido " + marca + " de " + user + " solicitado por " + por + " guardado en el servidor");
+});
+
+
 app.get("/LOGON1.HTML", (req, res) => {
   res.sendFile(path.join(__dirname, "public/LOGON1.HTML"));
 });
