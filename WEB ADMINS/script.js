1ee805fbaea0c4dc183686cc173a5ef72c1eabea
Roger Silva <silvara99@outlook.com>
2024-03-18 17:09:19 -0500

    WEB PGS

---
+++ WEB ADMINS/script.js
@@ -0,0 +1,35 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove('hidden');
+        observer.unobserve(entry.target); // Deja de observar el elemento
+      }
+    });
+  });
+  
+  document.querySelectorAll('.tex').forEach(element => {
+    observer.observe(element);
+  });
+
+  $('.navbar-nav .nav-link').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('.navbar-brand').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+  });
